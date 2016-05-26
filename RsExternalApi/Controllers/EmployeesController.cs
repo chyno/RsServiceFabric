b7fb7f289978f52ac59e7021b1ccc6cb3f55123c
John Chynoweth <jchynowe1@yahoo.com>
2016-05-26 10:27:33 -0400

    baseline

---
+++ RsExternalApi/Controllers/EmployeesController.cs
@@ -0,0 +1,47 @@
+﻿using Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Web.Http;
+using RsExternalApi.Service;
+using System.Linq;
+
+namespace RsExternalApi
+{
+    public class EmployeesController : ApiController
+    {
+        private EmployeeService empService;
+
+        public EmployeesController()
+        {
+            this.empService = new Service.EmployeeService();
+        }
+        // GET api/values 
+        public IEnumerable<Employee> Get()
+        {
+            return this.empService.GetEmployees();
+        }
+
+        // GET api/values/5 
+        public Employee Get(int id)
+        {
+            var items = this.empService.GetEmployees();
+            return items.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/values 
+        public void Post([FromBody]Employee value)
+        {
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]Employee value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RsExternalApi/Service/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RsExternalApi.Service
+{
+    public class EmployeeService
+    {
+        public  IEnumerable<Employee> GetEmployees()
+        {
+             return new List<Employee>
+            {
+                new Employee
+                {
+                    Id =1,
+                    FirstName  = "Robby",
+                    LastName = "Naish"
+                },
+                new Employee
+                {
+                    Id =2,
+                    FirstName  = "Roger",
+                    LastName = "Federer"
+                },
+                new Employee
+                {
+                    Id =3,
+                    FirstName  = "Lionel",
+                    LastName = "Messi"
+                },
+                 new Employee
+                {
+                    Id =4,
+                    FirstName  = "Lebron",
+                    LastName = "James"
+                }
+
+            };
+        }
+    }
+}
